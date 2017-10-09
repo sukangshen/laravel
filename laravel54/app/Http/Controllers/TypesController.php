c0f68cde7333fedd0c330c51b068b5eefb4a0ad6
1hmy <33808969@qq.com>
2017-10-09 19:39:10 +0800

    直播分类展示

--- laravel54/app/Http/Controllers/TypesController.php
+++ laravel54/app/Http/Controllers/TypesController.php
@@ -7,8 +7,21 @@ class TypesController extends Controller
 	//直播的首页
 	public function index()
 	{
-	return view('types/index');
+		$sql=DB::select('select * from live_class');
+        // print_r($sql);die;
+        return view("types/index",['data'=>$sql]);
 	}
+
+
+	  public function del(){
+        $id=$_GET['clas_id'];
+        $re=DB::table('live_class')->where('clas_id',$id)->delete();
+        if($re){
+            return redirect('types/index');
+        }else{
+            echo "删除失败";
+        }
+    }
 
 
 
