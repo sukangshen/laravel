41b309e5f33eedad9773876beb7fa0fb90f60166
1hmy <33808969@qq.com>
2017-10-09 20:55:09 +0800

    直播分类增删改查

--- laravel54/app/Http/Controllers/TypesController.php
+++ laravel54/app/Http/Controllers/TypesController.php
@@ -24,5 +24,35 @@ class TypesController extends Controller
     }
 
 
+    public function sets()
+	{
+		// echo "213";die;
+		$id = $_GET['clas_id'];
+		$re = DB::table('live_class')->where('clas_id',$id)->first();
+
+		// print_r($re);die;
+		return view('types/add_update',['re'=>$re]);
+	}
+
+	//修改的执行的方法
+	public function add_upd()
+	{
+
+		//接收要修改的名字，密码
+		$class_name=$_POST['class_name'];  
+        $class_addtime=$_POST['class_addtime'];  
+        $clas_id=$_POST['clas_id'];  
+        $arr=array('clas_id'=>$clas_id,'class_name'=>$class_name,'class_addtime'=>$class_addtime);  
+        $re=DB::table('live_class')  
+            ->where('clas_id','=',$clas_id )  
+            ->update($arr);  
+          if($re)  
+        {  
+            return redirect('types/index')->send();
+        }     
+    }  
+
+
+
 
 }
