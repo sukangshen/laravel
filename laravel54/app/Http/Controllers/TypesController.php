f123bf056c7c8d171d665e02c0a8aa3da21ba590
sukangshen <1226432372@qq.com>
2017-10-09 21:32:06 -0500

    前台用户中心

--- laravel54/app/Http/Controllers/TypesController.php
+++ laravel54/app/Http/Controllers/TypesController.php
@@ -7,51 +7,8 @@ class TypesController extends Controller
 	//直播的首页
 	public function index()
 	{
-		$sql=DB::select('select * from live_class');
-        // print_r($sql);die;
-        return view("types/index",['data'=>$sql]);
+	return view('types/index');
 	}
-
-
-	  public function del(){
-        $id=$_GET['clas_id'];
-        $re=DB::table('live_class')->where('clas_id',$id)->delete();
-        if($re){
-            return redirect('types/index');
-        }else{
-            echo "删除失败";
-        }
-    }
-
-
-    public function sets()
-	{
-		// echo "213";die;
-		$id = $_GET['clas_id'];
-		$re = DB::table('live_class')->where('clas_id',$id)->first();
-
-		// print_r($re);die;
-		return view('types/add_update',['re'=>$re]);
-	}
-
-	//修改的执行的方法
-	public function add_upd()
-	{
-
-		//接收要修改的名字，密码
-		$class_name=$_POST['class_name'];  
-        $class_addtime=$_POST['class_addtime'];  
-        $clas_id=$_POST['clas_id'];  
-        $arr=array('clas_id'=>$clas_id,'class_name'=>$class_name,'class_addtime'=>$class_addtime);  
-        $re=DB::table('live_class')  
-            ->where('clas_id','=',$clas_id )  
-            ->update($arr);  
-          if($re)  
-        {  
-            return redirect('types/index')->send();
-        }     
-    }  
-
 
 
 
--- laravel54/routes/web.php
+++ laravel54/routes/web.php
@@ -10,6 +10,19 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+$uri=$_SERVER['REQUEST_URI'];
+$uri=explode('/', $uri);
+if(isset($uri[2]))
+{
+  Route::any('/'.$uri[1].'/'.$uri[2], ucfirst($uri[1]).'Controller@'.$uri[2]);
+}else
+{
+  Route::any('/'.$uri[1], ucfirst($uri[1]).'Controller@index');
+}
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,8 +34,35 @@ route::any('index/add_do','IndexController@add_do');
 route::any('index/add_del','IndexController@add_del');
 route::any('index/add_update','IndexController@add_update');
 route::any('index/add_upd','IndexController@add_upd');
+
+
 route::any('show/index','ShowController@index');
 route::any('show/login','ShowController@login');
-route::any('show/login_do','ShowController@login_do');
-route::any('show/region','ShowController@region');
-route::any('show/region_do','ShowController@region_do');
+
+
+//直播分类
+route::any('types/index','TypesController@index');
+
+
+//个人中心
+route::any('person/index','PersonController@index');
+route::any('person/subscribe','PersonController@subscribe');
+route::any('person/profileauth','PersonController@profileauth');
+//配置路由规则
+// $ROUTES=isset($_GET['r'])?$_GET['r']:false;
+// if($ROUTES)
+// {
+//     $ROUTES=explode('/',$ROUTES);
+//     Route::match(['get','post'],'/',ucfirst($ROUTES[0]).'Controller@'.$ROUTES[1]);
+// }
+// else
+// {
+//     $uri=$_SERVER['REQUEST_URI'];
+//     $uri=explode('/', $uri);
+//     if(isset($uri[2])){
+//         Route::any('/'.$uri[1].'/'.$uri[2], ucfirst($uri[1]).'Controller@'.$uri[2]);
+//     }else{
+//         //Route::any('/'.$uri[2], ucfirst($uri[2]).'Controller@index');
+//         Route::any('/', 'LoginController@homepage');
+//     }
+// }
